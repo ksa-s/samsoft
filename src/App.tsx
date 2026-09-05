7447b387ff466b4aa823ba4e20b772ceb8e034ea
alsami456-oss <alsami456@gmail.com>
2026-09-06 01:13:41 +0300

    Revert "Fix styling and node modules setup"
    This reverts commit a9671123ead7229b943adc8ced4bdfce4f5501b9.

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ import { ErrorBoundary } from '@/components/error-boundary';
 import { Toaster } from '@/components/ui/toaster';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import NotFound from '@/pages/not-found';
-import JetourT2 from '@/pages/jetour-t2';
 import { Route, Switch, useLocation, Router as WouterRouter } from 'wouter';
 import { type ReactNode } from 'react';
 
@@ -387,7 +386,6 @@ function Router() {
     <RoutedErrorBoundary>
       <Switch>
         <Route path="/" component={Home} />
-        <Route path="/jetour-t2" component={JetourT2} />
         <Route component={NotFound} />
       </Switch>
     </RoutedErrorBoundary>
